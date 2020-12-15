156895824f67d233898a3d871bb0790b18dd135c
118140197 Neza Chania Putri <65174133+118140197-NezaChaniaPutri@users.noreply.github.com>
2020-12-15 08:16:11 +0700

    Tugas Modul 10

---
+++ Tugas2Dom.js
@@ -0,0 +1,5 @@
+console.log(document.getElementById("1"));
+console.log(document.getElementByTagName("button"));
+console.log(document.getElementByClassName("fungsi"));
+console.log(document.querySelector("li"));
+console.log(document.querySelectorAll("li"));
